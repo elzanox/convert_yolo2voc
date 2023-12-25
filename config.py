cd6405ce1023307cf661158abe771b9643937438
Immanuel <dimas@admin.spacearcade.online>
2023-12-25 23:51:20 +0700

    add

---
+++ config.py
@@ -0,0 +1,29 @@
+"""Configurations
+File paths to labels and image/xml files
+"""
+LABEL_DIR = "dataset_merged_full_24102023/labels_yolo"
+IMAGE_DIR = "dataset_merged_full_24102023/images"
+XML_DIR = "dataset_merged_full_24102023/labels_tf"
+
+names = [
+    "aeroplane",
+    "bicycle",
+    "bird",
+    "boat",
+    "bottle",
+    "bus",
+    "car",
+    "cat",
+    "chair",
+    "cow",
+    "diningtable",
+    "dog",
+    "horse",
+    "motorbike",
+    "person",
+    "pottedplant",
+    "sheep",
+    "sofa",
+    "train",
+    "tvmonitor",
+]
